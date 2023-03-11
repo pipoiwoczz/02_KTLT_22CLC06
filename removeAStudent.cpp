f6ad4f9492ad1392b82e288d02187736696476d4
khiemG921 <126588913+khiemG921@users.noreply.github.com>
2023-03-11 16:51:10 +0700

    removeAStudent

---
+++ removeAStudent.cpp
@@ -0,0 +1,24 @@
+#include "removeAStudent.h"
+
+void removeAStudent(Student*& pHead)
+{
+	Student* curr = pHead->next;
+	Student* prev = pHead;
+	int id;
+	cout << "Enter the ID of student: ";
+	cin >> id;
+	if (pHead->studentId == id) {
+		pHead = pHead->next;
+		delete pHead;
+		return;
+	}
+	while (prev) {
+		if (curr->studentId == id) {
+			prev->next = curr->next;
+			delete curr;
+			return;
+		}
+		prev = curr;
+		curr = curr->next;
+	}		
+}
---
+++ removeAStudent.h
@@ -0,0 +1,6 @@
+#ifndef _removeAStudent_
+#include <iostream>
+using namespace std;
+
+void removeAStudent(Student*& pHead);
+#endif
