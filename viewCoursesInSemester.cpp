219befa8699a2bc1a5775ec57392a28df1c15682
JackArrowden <87233227+JackArrowden@users.noreply.github.com>
2023-03-13 09:59:28 +0700

    upload .h files and view courses in semester

---
+++ viewCoursesInSemester.cpp
@@ -0,0 +1,10 @@
+#include "viewCoursesInSemester.h"
+
+void viewListCoursesInSem(Semester *semester) {
+    cout << "List of courses in this semester: " << endl;
+    Course *course = semester -> Courses;
+    while (course) {
+        cout << course -> courseName << endl;
+        course = course -> next;
+    }
+}
