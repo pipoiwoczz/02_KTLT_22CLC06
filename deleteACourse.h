7fa6b99f20551b4a6148b5dc6341893ce65f27fb
khiemG921 <126588913+khiemG921@users.noreply.github.com>
2023-03-15 22:32:26 +0700

    FUNCTION deleteACourse

---
+++ deleteACourse.h
@@ -0,0 +1,6 @@
+#ifndef _deleteACourse_
+#include <iostream>
+using namespace std;
+
+void deleteACourse(Course*& pHead);
+#endif
--- inputStudentsToCourse.h
+++ inputStudentsToCourse.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+using namespace std;
 
 void inputStudentsToCourse(Course*& pCourse);
 #endif
