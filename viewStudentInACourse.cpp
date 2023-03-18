514fe8cdf9e52b7db47830426bf163b015243a1f
JackArrowden <87233227+JackArrowden@users.noreply.github.com>
2023-03-18 23:06:46 +0700

    upload viewStudentInSCourse .cpp and .h

---
+++ viewStudentInACourse.cpp
@@ -0,0 +1,15 @@
+#include "viewStudentInACourse.h"
+
+void viewStudentInACourse(string s, string course) {
+    ifstream ifs;
+    s = s + course + ".txt";
+    string out;
+    ifs.open(s);
+    while (ifs) {
+        getline(ifs, out, ',');
+        cout << out << " ";
+        getline(ifs, out);
+        cout << out << endl;
+    }
+    ifs.close();
+}
