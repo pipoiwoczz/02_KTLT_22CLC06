414cea5f44c3f9e94d1e08d38b2d81280c843bdc
SoulGalaxy2312 <ngotri2312@gmail.com>
2023-04-10 11:27:43 +0700

    Fix a little, still wavering how to output in4 to profile/mssv.txt

--- Add1StToClass.cpp
+++ Add1StToClass.cpp
@@ -8,46 +8,68 @@
 using namespace std;
 
 void add1StToClass(Student *& headStudent, schoolYear * curSY, Class * curClass) {
-    Student * student = new Student;
+    Student stu;
     int no = 1;
 
     cout << "Enter student's ID: ";
-        cin >> student -> studentId;
-        cin.ignore();
-        cout << "Enter student's first name: ";
-        getline(cin , student -> firstName);
-        cin.ignore();
-        cout << "Enter student's last name: ";
-        getline(cin , student -> lastName);
-        cout << "Enter student's gender: ";
-        cin >> student -> gender;
-        cout << "Enter student's birthday: ";
-        cin >> student -> dateOfBirth;
-        cout << "Enter student's social ID: ";
-        cin >> student -> socialId;
+    cin >> stu.studentId;
+    cin.ignore();
+    cout << "Enter student's first name: ";
+    getline(cin , stu.firstName);
+    cin.ignore();
+    cout << "Enter student's last name: ";
+    getline(cin , stu.lastName);
+    cout << "Enter student's gender: ";
+    cin >> stu.gender;
+    cout << "Enter student's birthday: ";
+    cin >> stu.dateOfBirth;
+    cout << "Enter student's social ID: ";
+    cin >> stu.socialId;
 
-    string profilePath = "profile//" + to_string(student -> studentId) + ".txt";
     mkdir("profile");
-    ifstream fin(profilePath);
+    string path_Pro5_MSSV = "profile//" + to_string(stu.studentId) + ".txt";
+
+    ifstream fin(path_Pro5_MSSV);
     if (fin.is_open()) {
         cout << "This student has already exist in this schoolyear\n";
-        delete student;
         system("pause");
-        return add1StToClass(headStudent, curSY, curClass);
+
+        cout << "Do you want to continue adding 1 student to this class?";
+        cout << "\n1.Yes";
+        cout << "\n2.No";
+
+        int move;
+		
+		do	{
+			cout << "Please choose your move: ";
+			cin >> move;
+
+			switch (move)	{
+				case 1: 
+					return add1StToClass(headStudent, curSY, curClass);
+				case 2: 
+                    return;
+				default:	
+					cout << "You've entered wrong choice";
+					cout << "\nPlease try again";
+					cout << endl;
+			}	
+		}	while (move != 1 && move != 2);
     } else {
-        ofstream out(profilePath);
-        out << student -> studentId << endl << "1234\n";
+        // crete file profile/MSSV.txt
+        ofstream out(path_Pro5_MSSV);
+        out << stu.studentId << endl << "1234\n";
         out << curSY -> name << endl;
         out << curClass -> name << endl;
         out << 1 << endl;
-        out << student -> lastName << "," << student -> firstName << endl;
-        out << student -> gender << endl;
-        out << student -> dateOfBirth << endl;
-        out << student -> socialId;
+        out << stu.lastName << "," << stu.firstName << endl;
+        out << stu.gender << endl;
+        out << stu.dateOfBirth << endl;
+        out << stu.socialId;
         out.close();
     }
 
-    mkdir((curSY -> name + "//" + curClass -> name + "//" + to_string(student -> studentId)).c_str());
+    mkdir((curSY -> name + "//" + curClass -> name + "//" + to_string(stu.studentId)).c_str());
 
     string stPath = curSY -> name + "//" + curClass -> name + "//" + "student.txt";
     ifstream ifs(stPath);
@@ -56,22 +78,20 @@ void add1StToClass(Student *& headStudent, schoolYear * curSY, Class * curClass)
         string tmp;
         while (getline(ifs, tmp)) {
             ofs << tmp << endl;
-            if (tmp == to_string(student -> studentId)) {
+            if (tmp == to_string(stu.studentId)) {
                 cout << "This student has aldready exists\n\n";
                 ofs.close();
                 remove("tmp.txt");
                 ifs.close();
-                delete student;
                 system("pause");
                 return add1StToClass(headStudent, curSY, curClass);
             }
         }
-        ofs <<student -> studentId;
+        ofs <<stu.studentId;
     } else {
-        ofs <<student -> studentId;
+        ofs <<stu.studentId;
     }
 
-    delete student;
     ifs.close();
     ofs.close();
 
--- studentInAClass.cpp
+++ studentInAClass.cpp
@@ -5,102 +5,122 @@
 #include <iomanip>
 using namespace std;
 
-void inputStudentsToClass(schoolYear *curSY, Class * curClass)
+void inputStudentsToClass(string nameSY, string nameClass)
 {
-	int no = 1;
-	ifstream fin;
-	string path, temp;
-	cout << "Enter the path of a CSV file containing students: ";
-	cin >> path;
-	fin.open(path);
+	ifstream ifs;
+	string fileInput;
 
-	if (!fin.is_open()) {
+	cout << "Enter the path of a CSV file containing students: ";
+	cin >> fileInput;
+
+	if (!ifs.is_open()) {
+		ifs.close();
 		cout << "The file doesn't exist.";
-		return;
+		cout << "\nDo you want to continue adding students of class (by importing file)?";
+		cout << "\n1. Yes";
+		cout << "\n2. No";
+
+		int move;
+		
+		do	{
+			cout << "Please choose your move: ";
+			cin >> move;
+
+			switch (move)	{
+				case 1: 
+					return inputStudentsToClass(nameSY, nameClass);
+				case 2: return;
+				default:	
+					cout << "You've entered wrong choice";
+					cout << "\nPlease try again";
+					cout << endl;
+			}	
+		}	while (move != 1 && move != 2);
 	}
 
-	Student* curr = new Student;
+	Student tmp;
 
-	while (!fin.eof()) {
+	while (!ifs.eof()) {
 		string temp;
 
-		getline(fin, temp, ',');
-		getline(fin, temp, ',');
-		curr->studentId = stoi(temp);
-		getline(fin, temp, ',');
-		curr->firstName = temp;
-		getline(fin, temp, ',');
-		curr->lastName = temp;
-		getline(fin, temp, ',');
-		curr->gender = temp;
-		getline(fin, temp, ',');
-		curr->dateOfBirth = temp;
-		getline(fin, temp);
-		curr->socialId = temp;
+		getline(ifs, temp, ',');
+
+		getline(ifs, temp, ',');
+		tmp.studentId = stoi(temp);
+		getline(ifs, temp, ',');
+		tmp.firstName = temp;
+		getline(ifs, temp, ',');
+		tmp.lastName = temp;
+		getline(ifs, temp, ',');
+		tmp.gender = temp;
+		getline(ifs, temp, ',');
+		tmp.dateOfBirth = temp;
+		getline(ifs, temp);
+		tmp.socialId = temp;
 
 		mkdir("profile");
         ofstream ofs;
-        string path2 = "profile//" + to_string(curr->studentId) + ".txt";
+        string path_Pro5_MSSV = "profile/" + to_string(tmp.studentId) + ".txt";
 
-		string path3 = curSY -> name + "\\" + curClass -> name + "\\" + to_string(curr->studentId);
+		string path_SY_Class_MSSV = nameSY + "/" + nameClass + "/" + to_string(tmp.studentId);
 
 		// create folder MSSV in SY/Class
-		mkdir(path3.c_str());
+		mkdir(path_SY_Class_MSSV.c_str());
 
 		// add data to profile/mssv.txt
+		// Still deciding how to output in4 of student to file 
 
-        ofs.open(path2);
-		ofs << curr -> studentId << endl << "1234" << endl;
-		ofs << curSY -> name << endl;
-		ofs << curClass -> name << endl;
+        ofs.open(path_Pro5_MSSV);
+		ofs << tmp.studentId << endl << "1234" << endl;
+		ofs << nameSY << endl;
+		ofs << nameClass << endl;
 		ofs << 1 << endl;
-		ofs << curr -> lastName << "," << curr -> firstName << endl;
-		ofs << curr -> gender << endl;
-		ofs << curr -> dateOfBirth << endl;
-		ofs << curr -> socialId;
+		ofs << tmp.lastName << "," << tmp.firstName << endl;
+		ofs << tmp.gender << endl;
+		ofs << tmp.dateOfBirth << endl;
+		ofs << tmp.socialId;
         ofs.close();
 
 		// add data to file student.txt in folder SY//Class (only include student ID)
 
-		path3 = curSY -> name + "\\" + curClass -> name + "\\" + "student.txt";
-        ifstream ifs;
+		path_SY_Class_MSSV = nameSY + "/" + nameClass + "/" + "student.txt";
+        ifstream fin;
 
-        ifs.open(path3);
+        fin.open(path_SY_Class_MSSV);
         ofs.open("tmp.txt");
         if (ifs.is_open()) 
             while (getline(ifs, temp)) {
                 ofs << temp << endl;
             }
-		ofs << curr -> studentId;
-        ifs.close();
+		ofs << tmp.studentId;
+        fin.close();
         ofs.close();
-        remove(path3.c_str());
-        rename("tmp.txt", path3.c_str());
+        remove(path_SY_Class_MSSV.c_str());
+        rename("tmp.txt", path_SY_Class_MSSV.c_str());
 
 	}
-	delete curr;
 	
-	fin.close();
+	ifs.close();
 }
 
-void displayListOfStudents(string SY, string Class)
+void displayListOfStudents(string nameSY, string nameClass)
 {
-	string path = SY + "//" + Class + "//" + "student.txt";
+	string path = nameSY + "//" + nameClass + "//" + "student.txt";
 	string tmp;
 	int no = 1;
 
-	ifstream ifs(path);
+	ifstream ifs((path).c_str());
 
 	if (!ifs.is_open()) {
 		cout << "This class has no student now!!\n";
 		system("pause");
 		return;
 	} else {
-		string line;
-		while (getline(ifs, line)) {
-			cout << setw(3) << no ;
-			no++;
-			ifstream fin("profile//" + line + ".txt");
+		string mssv;
+		while (getline(ifs, mssv)) {
+			cout << setw(3) << no;
+			++no;
+			ifstream fin(("profile//" + mssv + ".txt").c_str());
 			if (fin.is_open()) {
 				string tmp;
 				getline(fin, tmp);
