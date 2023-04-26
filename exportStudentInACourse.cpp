76aac95df4069ad8ba3bab18e4f57252cce6acf8
SoulGalaxy2312 <ngotri2312@gmail.com>
2023-04-26 10:02:29 +0700

    Update Func 9 Course Menu
    Export Students in a course (all classes)

--- exportStudentInACourse.cpp
+++ exportStudentInACourse.cpp
@@ -3,65 +3,42 @@
 void exportStudentInACourse(string SY, string Sem, string course)   {
 	string outputPath;
 
-	cout << "Input path: ";
+	cout << "Input path to export CSV file: ";
 	cin.ignore();
 	getline(cin, outputPath);
 
 	ofstream ofs;
-
+	
 	ofs.open(outputPath);
 
 	if (ofs.is_open())	{
-		string path; 
-		ifstream courseClass;
+		string path = "./" + SY + "/" + Sem + "/" + course + "/class.txt";
+		string courseClass, studentID, studentName;
+		ifstream ifs, Class;
 
-		path = "./" + SY + "/" + Sem + "/" + course + "/class.txt";
-		
-		string cClass;
-		courseClass.open(path);
-			while (!courseClass.eof()) {
-				getline(courseClass, cClass);
-				ofs << course << " class " << cClass << endl;
+		int i=1; // Ordinal number
 
-				ifstream ifs;
-				ifs.open("./" + SY + "/" + Sem + "/" + course + "/" + cClass + "/scoreboard.txt");
-					if (ifs.is_open())	{
-						ofs << "No" << "," << "ID" << ",";
-						ofs << "Last name" << "," << "First Name" << ",";
-						ofs << "Total" << "," << "Final" << "," << "Midterm" << "," << "Other" << endl;
-						while (!ifs.eof()) {
-							string tmp;
+		ifs.open(path);
+			while (!ifs.eof()) {
+				getline(ifs, courseClass);
+				ofs << course << " class " << courseClass << endl;
+				ofs << "No" << "," << "ID" << "," << "Class" << "," << "Name" << endl;
 
-							getline(ifs, tmp, ','); // Ordinal number
-							ofs << left << tmp << ",";
-							getline(ifs, tmp, ','); // ID
-							ofs << left << tmp << ",";
-							getline(ifs, tmp, ','); // Last Name
-							ofs << tmp << ",";
-							getline(ifs, tmp, ','); // First Name
-							ofs << tmp << ",";
-							getline(ifs, tmp, ','); // Total
-							ofs << tmp << ",";
-							getline(ifs, tmp, ','); // Final 
-							ofs << tmp << ",";
-							getline(ifs, tmp, ','); // Midterm
-							ofs << tmp << ",";
-							getline(ifs, tmp); // Other
-							ofs << tmp << endl;
-						}
+				Class.open("./" + SY + "/" + Sem + "/" + course + "/" + courseClass + "/listStud.txt");
+					while (!Class.eof()) {
+						getline(Class, studentID, ',');
+						getline(Class, studentName);
+						ofs << i << "," << studentID << "," << courseClass << "," << studentName << endl;
+						i++;
 					}
-					else	{
-						ofs << "This class doesn't have a scoreboard yet.\n";
-						ofs << "Please import a scoreboard first.\n";
-					}
-
-				ifs.close();
+				Class.close();
+				ofs << endl;
 			}
-		courseClass.close();
-	} else	{
-		cout << "You've entered wrong path.\n";
-		cout << "Please try again.\n";
+		ifs.close();
+	}	else	{
+		cout << "You've entered wrong path\n";
+		cout << "Please try again\n";
 	}
-    
+
 	ofs.close();
 }
