b52a542a92862c8fdfbec65d1438e031f05357d9
SoulGalaxy2312 <ngotri2312@gmail.com>
2023-04-26 09:08:00 +0700

    Update function
    Export Students in a course (all classes)

--- exportStudentInACourse.cpp
+++ exportStudentInACourse.cpp
@@ -1,78 +1,67 @@
 #include "exportStudentInACourse.h"
 
 void exportStudentInACourse(string SY, string Sem, string course)   {
-    // choose Course Class to view students  
-	string cClass, path, chosenClass;
-	ifstream courseClass;
-	path = "./" + SY + "/" + Sem + "/" + course + "/class.txt";
+	string outputPath;
 
-	// View list of classes in a course
-	courseClass.open(path);
-		cout << "---------List of Class in this Course------------\n";
-		while (!courseClass.eof()) {
-			getline(courseClass, cClass);
-			cout << cClass << endl;
-		}
-		cout << "---------------------------------------------------\n";
-	courseClass.close();
+	cout << "Input path: ";
+	cin.ignore();
+	getline(cin, outputPath);
 
-	cout << "Enter class's name: ";
-	cin >> chosenClass;
+	ofstream ofs;
 
-	// Check whether the input is correct or not
-	bool exist = false;
-	courseClass.open(path);
-		while (!courseClass.eof()) {
-			getline(courseClass, cClass);
-			if (cClass == chosenClass) {
-				exist = true;
-				break;
+	ofs.open(outputPath);
+
+	if (ofs.is_open())	{
+		string path; 
+		ifstream courseClass;
+
+		path = "./" + SY + "/" + Sem + "/" + course + "/class.txt";
+		
+		string cClass;
+		courseClass.open(path);
+			while (!courseClass.eof()) {
+				getline(courseClass, cClass);
+				ofs << course << " class " << cClass << endl;
+
+				ifstream ifs;
+				ifs.open("./" + SY + "/" + Sem + "/" + course + "/" + cClass + "/scoreboard.txt");
+					if (ifs.is_open())	{
+						ofs << "No" << "," << "ID" << ",";
+						ofs << "Last name" << "," << "First Name" << ",";
+						ofs << "Total" << "," << "Final" << "," << "Midterm" << "," << "Other" << endl;
+						while (!ifs.eof()) {
+							string tmp;
+
+							getline(ifs, tmp, ','); // Ordinal number
+							ofs << left << tmp << ",";
+							getline(ifs, tmp, ','); // ID
+							ofs << left << tmp << ",";
+							getline(ifs, tmp, ','); // Last Name
+							ofs << tmp << ",";
+							getline(ifs, tmp, ','); // First Name
+							ofs << tmp << ",";
+							getline(ifs, tmp, ','); // Total
+							ofs << tmp << ",";
+							getline(ifs, tmp, ','); // Final 
+							ofs << tmp << ",";
+							getline(ifs, tmp, ','); // Midterm
+							ofs << tmp << ",";
+							getline(ifs, tmp); // Other
+							ofs << tmp << endl;
+						}
+					}
+					else	{
+						ofs << "This class doesn't have a scoreboard yet.\n";
+						ofs << "Please import a scoreboard first.\n";
+					}
+
+				ifs.close();
 			}
-		}		
-	courseClass.close();
-
-	if (!exist) {
-		cout << "You've entered an incorrect class name!\n";
-		cout << "Enter 0 to get back to previous menu or any of other keys to enter class name again\n";
-		cout << "Your choice: ";
-		string temp;
-		cin >> temp;
-    	cout << "--------------------" << endl;
-        return;
+		courseClass.close();
+	} else	{
+		cout << "You've entered wrong path.\n";
+		cout << "Please try again.\n";
 	}
-
-    string out;
-    ifstream ifs;
-        ifs.open("./" + SY + "/" + Sem + "/" + course + "/" + chosenClass + "/listStud.txt");
-		if (ifs.is_open()) {
-            ofstream ofs;
-            string outputPath;
-
-            cout << "Input path to export CSV file: ";
-            cin.ignore();
-            getline(cin, outputPath);
-
-            ofs.open(outputPath);
-            if (ofs)    {
-                int i = 1; // Ordinal number
-                ofs << course << " class " << chosenClass << endl;
-                ofs << left << setw(4) << "No." << setw(10) << "ID" << "Name" << endl;
-                while (!ifs.eof()) {
-                    ofs << left << setw(4) << i; // Ordinal number
-                    getline(ifs, out, ',');
-                    ofs << setw(10) << out; // Id
-                    getline(ifs, out);
-                    ofs << out << endl; // Name
-                    i++;
-                }
-		    }
-        
-            ofs.close();  
-            }
-			
-		else {
-			cout << "This Course Class has no students yet.\n";
-            cout << "Please add students to this course class first.\n";
-		}
-    ifs.close();
+    
+	ofs.close();
 }
