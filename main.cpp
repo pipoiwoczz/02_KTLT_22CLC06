806881464f3b3c6b3dfe66c38a592bf2366caec0
pipoiwoczz <lengocanhkhoa2919@gmail.com>
2023-04-27 18:31:59 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
-#include "mainmenu.h"
-#include "main.h"
-#include "loadProgress.h"
-#include "saveStudentScoreboard.h"
-#include "menuClass.h"
-using namespace std;
-
-
+#include "gui.h"
+#include "guiMenu.h"
+#include "guiView.h"
+#include "guiAddnRemove.h"
 int main() {
+	system("cls");
+	textSize(24);
+	fixConsoleWindow();
 
-    mainMenu();
-
-    return 0;
+	//viewCourseInformation("khoa", "2022-2023", 1, "CSS101");
+	//viewAndChooseCourseClass("khoa", "2022-2023", 1, "CSS101");
+	mainmenuOpt();
+	return 1;
 }
